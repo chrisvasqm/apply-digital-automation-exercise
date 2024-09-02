fa91cca6311c9147ca633f2111823fcfa14a9704
chrisvasqm <christianvasquezmelo@gmail.com>
2024-09-02 19:36:19 -0400

    Make test for multiple viewports

--- cypress/e2e/exercise.cy.js
+++ cypress/e2e/exercise.cy.js
@@ -2,7 +2,24 @@ import { faker } from '@faker-js/faker';
 import generateRandomNumber from '../../common/generate-random-number';
 import dayjs from 'dayjs';
 
+/**
+ * Used some of the most common mobile and desktop viewports using
+ * a preset and a predefined resolution.
+ * 
+ * For more info, refer to https://docs.cypress.io/api/commands/viewport
+ */
+const viewports = ['iphone-x', [1920, 1080]];
+
+const setViewport = (viewport) => {
+  if (Array.isArray(viewport) && viewport.length == 2) {
+    cy.viewport(viewport[0], viewport[1]);
+  } else {
+    cy.viewport(viewport);
+  }
+}
+
 describe('Automation Exercise', () => {
+
   describe('Checkout', () => {
     beforeEach(() => {
       cy.visit(Cypress.env('baseUrl'));
@@ -26,22 +43,26 @@ describe('Automation Exercise', () => {
       cy.get('b').should('exist');
     });
 
-    it('should be able to place an order as a new Customer', () => {
-      cy.get('.nav > :nth-child(2) > a').click();
-      cy.get(':nth-child(5) > .product-image-wrapper > .choose > .nav > li > a').click();
-      cy.get('#quantity').clear().type(generateRandomNumber());
-      cy.get(':nth-child(5) > .btn').click();
-      cy.get('u').click();
-      cy.get('.col-sm-6 > .btn').click();
-      cy.get(':nth-child(7) > .btn').click();
-      cy.get('[data-qa="name-on-card"]').type(faker.person.fullName());
-      cy.get('[data-qa="card-number"]').type(faker.finance.creditCardNumber());
-      cy.get('[data-qa="cvc"]').type(faker.finance.creditCardCVV());
-      cy.get('[data-qa="expiry-month"]').type(dayjs().month());
-      cy.get('[data-qa="expiry-year"]').type(dayjs().year());
-      cy.get('[data-qa="pay-button"]').click();
-      cy.get('[data-qa="order-placed"] > b').should('exist');
-    });
+    viewports.forEach(viewport => {
+      it(`should be able to place an order as a new Customer on ${viewport}`, () => {
+        setViewport(viewport);
+
+        cy.get('.nav > :nth-child(2) > a').click();
+        cy.get(':nth-child(5) > .product-image-wrapper > .choose > .nav > li > a').click();
+        cy.get('#quantity').clear().type(generateRandomNumber());
+        cy.get(':nth-child(5) > .btn').click();
+        cy.get('u').click();
+        cy.get('.col-sm-6 > .btn').click();
+        cy.get(':nth-child(7) > .btn').click();
+        cy.get('[data-qa="name-on-card"]').type(faker.person.fullName());
+        cy.get('[data-qa="card-number"]').type(faker.finance.creditCardNumber());
+        cy.get('[data-qa="cvc"]').type(faker.finance.creditCardCVV());
+        cy.get('[data-qa="expiry-month"]').type(dayjs().month());
+        cy.get('[data-qa="expiry-year"]').type(dayjs().year());
+        cy.get('[data-qa="pay-button"]').click();
+        cy.get('[data-qa="order-placed"] > b').should('exist');
+      });
+    })
 
     afterEach(() => {
       cy.get('.nav > :nth-child(4) > a').click();
